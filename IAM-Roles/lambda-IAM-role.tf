09839f73ce2afba5cc9d23753b3ed0047c4992e4
samer-habash <sam.habash1984@gmail.com>
2020-05-21 18:32:21 +0300

    deleted app.zip

--- IAM-Roles/lambda-IAM-role.tf
+++ IAM-Roles/lambda-IAM-role.tf
@@ -1,3 +1,7 @@
+//data "aws_iam_role" "check_existence_iam_for_lambda" {
+//  name = aws_iam_role.iam_for_lambda.name
+//}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
     assume_role_policy = <<EOF
